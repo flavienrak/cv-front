b35f24615ed29fc8d2a699125a20f0216c2d09cb
flavienrak <flavien.andrisoarak@gmail.com>
2025-03-28 17:59:01 +0300

    feat(installation): finish environment installation

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import HomeComponent from '@/components/home/HomeComponent';
+
+export default function Home() {
+  return (
+    <div className="relative w-full min-h-screen">
+      <HomeComponent />
+    </div>
+  );
+}
